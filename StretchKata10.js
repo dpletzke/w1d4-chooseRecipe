5a4c588bfb5742b6298e629dae6bb62829af2c29
Dan Pletzke <dpletzke@gmail.com>
2020-09-17 20:43:32 +0000

    original solution

---
+++ StretchKata10.js
@@ -0,0 +1,49 @@
+//StretchKata10
+//for lighthouse labs prep modules
+
+const chooseRecipe = function(bakeryA, bakeryB, recipes) {
+  let output;
+  for (recipe of recipes){
+    if(bakeryA.includes(recipe.ingredients[0]) && bakeryB.includes(recipe.ingredients[1])){ return recipe.name }  //check one option
+    if(bakeryB.includes(recipe.ingredients[0]) && bakeryA.includes(recipe.ingredients[1])){ return recipe.name }  //check the other option
+  }
+ return undefined;
+}
+
+let bakeryA = ['saffron', 'eggs', 'tomato paste', 'coconut', 'custard'];
+let bakeryB = ['milk', 'butter', 'cream cheese'];
+let recipes = [
+    {
+        name: 'Coconut Sponge Cake',
+        ingredients: ['coconut', 'cake base']
+    },
+    {
+        name: 'Persian Cheesecake',
+        ingredients: ['saffron', 'cream cheese']
+    },
+    {
+        name: 'Custard Surprise',
+        ingredients: ['custard', 'ground beef']
+    }
+];
+
+console.log(chooseRecipe(bakeryA, bakeryB, recipes));
+
+bakeryA = ['potatoes', 'bay leaf', 'raisins'];
+bakeryB = ['red bean', 'dijon mustard', 'apples'];
+recipes = [
+    {
+        name: 'Potato Ganache',
+        ingredients: ['potatoes', 'chocolate']
+    },
+    {
+        name: 'Sweet Fish',
+        ingredients: ['anchovies', 'honey']
+    },
+    {
+        name: "Nima's Famous Dijon Raisins",
+        ingredients: ['dijon mustard', 'raisins']
+    }
+];
+
+console.log(chooseRecipe(bakeryA, bakeryB, recipes));
